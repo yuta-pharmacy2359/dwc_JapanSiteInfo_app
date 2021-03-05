e7bce5a3c80e1e608019391bc550d2407a0823df
Yuta <yuta.shogai@gmail.com>
2021-03-05 16:26:38 +0000

    ユーザー詳細・編集画面作成、ユーザーのバリデーション、フラッシュメッセージ日本語化

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bdba947ed64c24cdbc029884791dca7eeeba2eac26ddd91da717cd8e8d31256db27bf0dd95e0cae9c8eacf8446d598f7677ab7a791cea323ff33ca3ca466eab3'

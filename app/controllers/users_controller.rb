875d0590d8f2cebe2a340323de51a57ef4ddc1b2
Yuta <yuta.shogai@gmail.com>
2021-03-06 13:52:06 +0000

    ページネーション実装、マイページ一部修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user= User.find(params[:id])
-    @spots = @user.spots
+    @spots = @user.spots.page(params[:page]).reverse_order
   end
 
   def index

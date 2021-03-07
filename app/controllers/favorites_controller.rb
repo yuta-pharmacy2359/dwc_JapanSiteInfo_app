d00d4a7653f60ed9449b1113410ca43a70ec71cf
Yuta <yuta.shogai@gmail.com>
2021-03-07 08:07:00 +0000

    いいね機能(同期)実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+
+  def create
+    @spot = Spot.find(params[:spot_id])
+    @favorite = current_user.favorites.new(spot_id: @spot.id)
+    @favorite.save
+    redirect_back(fallback_location: top_path)
+  end
+
+  def destroy
+    @spot = Spot.find(params[:spot_id])
+    @favorite = current_user.favorites.find_by(spot_id: @spot.id)
+    @favorite.destroy
+    redirect_back(fallback_location: top_path)
+  end
+
 end

320a51f3a8c216c0e4ed365f9088c403f545a61c
Yuta <yuta.shogai@gmail.com>
2021-03-07 06:27:06 +0000

    いいね機能　モデル・コントローラー作成ほか

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :spots, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   attachment :profile_image
 

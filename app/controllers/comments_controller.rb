f02a66da80be6f222f5e3730bb328739108df6fb
Yuta <yuta.shogai@gmail.com>
2021-03-07 11:09:18 +0000

    非同期版いいね実装、コメント機能一部修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, { only: [:create] }
   before_action :baria_user, { only: [:destroy] }
 
   def create
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,18 @@ class FavoritesController < ApplicationController
 
   def create
     @spot = Spot.find(params[:spot_id])
-    @favorite = current_user.favorites.new(spot_id: @spot.id)
+    if current_user.nil?
+      @favorite = Favorite.new(spot_id: @spot.id)
+    else
+      @favorite = current_user.favorites.new(spot_id: @spot.id)
+    end
     @favorite.save
-    redirect_back(fallback_location: top_path)
   end
 
   def destroy
     @spot = Spot.find(params[:spot_id])
     @favorite = current_user.favorites.find_by(spot_id: @spot.id)
     @favorite.destroy
-    redirect_back(fallback_location: top_path)
   end
 
 end

05d75ef8799a722a4423ffd457ed12e49a6300ee
Yuta <yuta.shogai@gmail.com>
2021-03-08 05:45:01 +0000

    非同期版フォロー機能完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,12 +8,24 @@ class FavoritesController < ApplicationController
       @favorite = current_user.favorites.new(spot_id: @spot.id)
     end
     @favorite.save
+    @user= @spot.user
+    @spots = @user.spots.page(params[:page]).reverse_order
+    @favorites_count = 0
+    @spots.each do |spot|
+      @favorites_count += spot.favorites.count
+    end
   end
 
   def destroy
     @spot = Spot.find(params[:spot_id])
     @favorite = current_user.favorites.find_by(spot_id: @spot.id)
     @favorite.destroy
+    @user= @spot.user
+    @spots = @user.spots.page(params[:page]).reverse_order
+    @favorites_count = 0
+    @spots.each do |spot|
+      @favorites_count += spot.favorites.count
+    end
   end
 
 end
--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -3,13 +3,11 @@ class FollowRelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    redirect_back(fallback_location: top_path)
   end
 
   def destroy
     @user = FollowRelationship.find(params[:id]).followed
     current_user.unfollow(@user)
-    redirect_back(fallback_location: top_path)
   end
 
 end

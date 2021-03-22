93ab10c9ee66fcb4e96b795bf558fa7826ccf738
Yuta <yuta.shogai@gmail.com>
2021-03-22 11:53:41 +0000

    テスト バリデーション1

--- spec/system/03_finishing_touches_spec.rb
+++ spec/system/03_finishing_touches_spec.rb
@@ -3,172 +3,219 @@ require 'rails_helper'
 describe '[STEP3] 仕上げのテスト' do
   let(:user) { create(:user) }
   let!(:other_user) { create(:user) }
-  let!(:book) { create(:book, user: user) }
-  let!(:other_book) { create(:book, user: other_user) }
+  let!(:spot) { create(:spot, user: user) }
+  let!(:other_spot) { create(:spot, user: other_user) }
 
   describe 'サクセスメッセージのテスト' do
     subject { page }
 
     it 'ユーザ新規登録成功時' do
       visit new_user_registration_path
-      fill_in 'user[name]', with: Faker::Lorem.characters(number: 9)
-      fill_in 'user[email]', with: 'a' + user.email # 確実にuser, other_userと違う文字列にするため
+      fill_in 'user[fullname]', with: Faker::Lorem.characters(number: 10)
+      fill_in 'user[nickname]', with: Faker::Lorem.characters(number: 10)
+      fill_in 'user[email]', with: 'a' + user.email
+      choose('user_sex_male')
+      fill_in 'user[birthday]', with: '2000-01-01'
+      select '東京都', from: 'user_prefecture'
+      fill_in 'user[city]', with: Faker::Lorem.characters(number: 10)
       fill_in 'user[password]', with: 'password'
       fill_in 'user[password_confirmation]', with: 'password'
-      click_button 'Sign up'
-      is_expected.to have_content 'successfully'
+      click_button '新規登録'
+      is_expected.to have_content 'ようこそ！ アカウントが登録されました'
     end
     it 'ユーザログイン成功時' do
       visit new_user_session_path
-      fill_in 'user[name]', with: user.name
+      fill_in 'user[email]', with: user.email
       fill_in 'user[password]', with: user.password
-      click_button 'Log in'
-      is_expected.to have_content 'successfully'
+      click_button 'ログイン'
+      is_expected.to have_content 'ログインしました'
     end
     it 'ユーザログアウト成功時' do
       visit new_user_session_path
-      fill_in 'user[name]', with: user.name
+      fill_in 'user[email]', with: user.email
       fill_in 'user[password]', with: user.password
-      click_button 'Log in'
-      logout_link = find_all('a')[4].native.inner_text
+      click_button 'ログイン'
+      logout_link = find_all('a')[9].native.inner_text
       logout_link = logout_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
       click_link logout_link
-      is_expected.to have_content 'successfully'
+      is_expected.to have_content 'ログアウトしました'
     end
     it 'ユーザのプロフィール情報更新成功時' do
       visit new_user_session_path
-      fill_in 'user[name]', with: user.name
+      fill_in 'user[email]', with: user.email
       fill_in 'user[password]', with: user.password
-      click_button 'Log in'
+      click_button 'ログイン'
       visit edit_user_path(user)
-      click_button 'Update User'
-      is_expected.to have_content 'successfully'
+      click_button '更新する'
+      is_expected.to have_content 'プロフィールを更新しました'
     end
-    it '投稿データの新規投稿成功時: 投稿一覧画面から行います。' do
+    it '投稿データの新規投稿成功時' do
       visit new_user_session_path
-      fill_in 'user[name]', with: user.name
+      fill_in 'user[email]', with: user.email
       fill_in 'user[password]', with: user.password
-      click_button 'Log in'
-      visit books_path
-      fill_in 'book[title]', with: Faker::Lorem.characters(number: 5)
-      fill_in 'book[body]', with: Faker::Lorem.characters(number: 20)
-      click_button 'Create Book'
-      is_expected.to have_content 'successfully'
+      click_button 'ログイン'
+      visit new_spot_path
+      fill_in 'spot[title]', with: Faker::Lorem.characters(number: 10)
+      select '東京都', from: 'spot_prefecture'
+      fill_in 'spot[city]', with: Faker::Lorem.characters(number: 10)
+      fill_in 'spot[visited_day]', with: '2021-01-01'
+      #fill_in 'spot[rate]', with: '5', visible: false
+      #写真(1〜3枚目)の入力
+
+
+      fill_in 'spot[content]', with: Faker::Lorem.characters(number: 50)
+      click_button '投稿する'
+      is_expected.to have_content 'スポットを投稿しました'
     end
     it '投稿データの更新成功時' do
       visit new_user_session_path
-      fill_in 'user[name]', with: user.name
+      fill_in 'user[email]', with: user.email
       fill_in 'user[password]', with: user.password
-      click_button 'Log in'
-      visit edit_book_path(book)
-      click_button 'Update Book'
-      is_expected.to have_content 'successfully'
+      click_button 'ログイン'
+      visit edit_spot_path(spot)
+      click_button '更新する'
+      is_expected.to have_content 'スポットを更新しました'
     end
   end
 
   describe '処理失敗時のテスト' do
-    context 'ユーザ新規登録失敗: nameを1文字にする' do
+    context 'ユーザ新規登録失敗: 誕生日を未来の日付にする' do
       before do
         visit new_user_registration_path
-        @name = Faker::Lorem.characters(number: 1)
-        @email = 'a' + user.email # 確実にuser, other_userと違う文字列にするため
-        fill_in 'user[name]', with: @name
+        @fullname = Faker::Lorem.characters(number: 10)
+        @nickname = Faker::Lorem.characters(number: 10)
+        @email = 'a' + user.email
+        @sex = 'user_sex_male'
+        @birthday = '2112-01-01'
+        @prefecture = '東京都'
+        @city = Faker::Lorem.characters(number: 10)
+        fill_in 'user[fullname]', with: @fullname
+        fill_in 'user[nickname]', with: @nickname
         fill_in 'user[email]', with: @email
+        choose(@sex)
+        fill_in 'user[birthday]', with: @birthday
+        select @prefecture, from: 'user_prefecture'
+        fill_in 'user[city]', with: @city
         fill_in 'user[password]', with: 'password'
         fill_in 'user[password_confirmation]', with: 'password'
       end
 
       it '新規登録されない' do
-        expect { click_button 'Sign up' }.not_to change(User.all, :count)
+        expect { click_button '新規登録' }.not_to change(User.all, :count)
       end
       it '新規登録画面を表示しており、フォームの内容が正しい' do
-        click_button 'Sign up'
-        expect(page).to have_content 'Sign up'
-        expect(page).to have_field 'user[name]', with: @name
+        click_button '新規登録'
+        expect(page).to have_content '新規登録'
+        expect(page).to have_field 'user[fullname]', with: @fullname
+        expect(page).to have_field 'user[nickname]', with: @nickname
         expect(page).to have_field 'user[email]', with: @email
+        expect(page).to have_checked_field @sex
+        expect(page).to have_field 'user[birthday]', with: @birthday
+        expect(page).to have_select('都道府県', selected: @prefecture)
+        expect(page).to have_field 'user[city]', with: @city
       end
       it 'バリデーションエラーが表示される' do
-        click_button 'Sign up'
-        expect(page).to have_content "is too short (minimum is 2 characters)"
+        click_button '新規登録'
+        expect(page).to have_content "誕生日が無効な日付です"
       end
     end
 
-    context 'ユーザのプロフィール情報編集失敗: nameを1文字にする' do
+    context 'ユーザのプロフィール情報編集失敗: 誕生日を未来の日付にする' do
       before do
-        @user_old_name = user.name
-        @name = Faker::Lorem.characters(number: 1)
+        @user_old_birthday = user.birthday
+        @birthday = "2112-01-01"
         visit new_user_session_path
-        fill_in 'user[name]', with: @user_old_name
+        fill_in 'user[email]', with: user.email
         fill_in 'user[password]', with: user.password
-        click_button 'Log in'
+        click_button 'ログイン'
         visit edit_user_path(user)
-        fill_in 'user[name]', with: @name
-        click_button 'Update User'
+        fill_in 'user[birthday]', with: @birthday
+        click_button '更新する'
       end
 
       it '更新されない' do
-        expect(user.reload.name).to eq @user_old_name
+        expect(user.reload.birthday).to eq @user_old_birthday
       end
       it 'ユーザ編集画面を表示しており、フォームの内容が正しい' do
-        expect(page).to have_field 'user[name]', with: @name
+        expect(page).to have_field 'user[fullname]', with: user.fullname
+        expect(page).to have_field 'user[nickname]', with: user.nickname
+        expect(page).to have_field 'user[email]', with: user.email
+        expect(page).to have_checked_field 'edit_user_sex_male'
+        expect(page).to have_field 'user[birthday]', with: @birthday
+        expect(page).to have_select('都道府県', selected: '東京都')
+        expect(page).to have_field 'user[city]', with: user.city
+        #expect(page).to have_field 'user[profile_image]', with: user.profile_image
+        expect(page).to have_field 'user[introduction]', with: user.introduction
       end
       it 'バリデーションエラーが表示される' do
-        expect(page).to have_content "is too short (minimum is 2 characters)"
+        expect(page).to have_content "誕生日が無効な日付です"
       end
     end
 
-    context '投稿データの新規投稿失敗: 投稿一覧画面から行い、titleを空にする' do
+    context '投稿データの新規投稿失敗: タイトルを空にする' do
       before do
         visit new_user_session_path
-        fill_in 'user[name]', with: user.name
+        fill_in 'user[email]', with: user.email
         fill_in 'user[password]', with: user.password
-        click_button 'Log in'
-        visit books_path
-        @body = Faker::Lorem.characters(number: 19)
-        fill_in 'book[body]', with: @body
+        click_button 'ログイン'
+        visit new_spot_path
+        @city = Faker::Lorem.characters(number: 10)
+        @visited_day = '2021-01-01'
+        @content = Faker::Lorem.characters(number: 50)
+        select '東京都', from: 'spot_prefecture'
+        fill_in 'spot[city]', with: @city
+        fill_in 'spot[visited_day]', with: @visited_day
+        #fill_in 'spot[rate]', with: '5', visible: false
+        #写真(1〜3枚目)の入力
+
+
+        fill_in 'spot[content]', with: @content
       end
 
       it '投稿が保存されない' do
-        expect { click_button 'Create Book' }.not_to change(Book.all, :count)
-      end
-      it '投稿一覧画面を表示している' do
-        click_button 'Create Book'
-        expect(current_path).to eq '/books'
-        expect(page).to have_content book.body
-        expect(page).to have_content other_book.body
+        expect { click_button '投稿する' }.not_to change(Spot.all, :count)
       end
       it '新規投稿フォームの内容が正しい' do
-        expect(find_field('book[title]').text).to be_blank
-        expect(page).to have_field 'book[body]', with: @body
+        click_button '投稿する'
+        expect(find_field('spot[title]').text).to be_blank
+        expect(page).to have_select('都道府県', selected: '東京都')
+        expect(page).to have_field 'spot[city]', with: @city
+        expect(page).to have_field 'spot[visited_day]', with: @visited_day
+        #写真1〜3枚目
+        expect(page).to have_field 'spot[content]', with: @content
       end
       it 'バリデーションエラーが表示される' do
-        click_button 'Create Book'
-        expect(page).to have_content "can't be blank"
+        click_button '投稿する'
+        expect(page).to have_content "タイトルを入力してください"
       end
     end
 
     context '投稿データの更新失敗: titleを空にする' do
       before do
         visit new_user_session_path
-        fill_in 'user[name]', with: user.name
+        fill_in 'user[email]', with: user.email
         fill_in 'user[password]', with: user.password
-        click_button 'Log in'
-        visit edit_book_path(book)
-        @book_old_title = book.title
-        fill_in 'book[title]', with: ''
-        click_button 'Update Book'
+        click_button 'ログイン'
+        visit edit_spot_path(spot)
+        @spot_old_title = spot.title
+        fill_in 'spot[title]', with: ''
+        click_button '更新する'
       end
 
-      it '投稿が更新されない' do
-        expect(book.reload.title).to eq @book_old_title
+      it 'スポットが更新されない' do
+        expect(spot.reload.title).to eq @spot_old_title
       end
-      it '投稿編集画面を表示しており、フォームの内容が正しい' do
-        expect(current_path).to eq '/books/' + book.id.to_s
-        expect(find_field('book[title]').text).to be_blank
-        expect(page).to have_field 'book[body]', with: book.body
+      it 'スポット編集画面を表示しており、フォームの内容が正しい' do
+        expect(current_path).to eq '/spots/' + spot.id.to_s
+        expect(find_field('spot[title]').text).to be_blank
+        expect(page).to have_select('都道府県', selected: '東京都')
+        expect(page).to have_field 'spot[city]', with: spot.city
+        expect(page).to have_field 'spot[visited_day]', with: spot.visited_day
+        #写真1〜3枚目
+        expect(page).to have_field 'spot[content]', with: spot.content
       end
       it 'エラーメッセージが表示される' do
-        expect(page).to have_content 'error'
+        expect(page).to have_content 'タイトルを入力してください'
       end
     end
   end

fd630742de1128f16d5d3fb225a89672133ed501
Yuta <yuta.shogai@gmail.com>
2021-03-29 10:06:19 +0000

    細かなバグ修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_03_08_080245) do
+
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "spot_id"
@@ -83,4 +84,5 @@ ActiveRecord::Schema.define(version: 2021_03_08_080245) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
